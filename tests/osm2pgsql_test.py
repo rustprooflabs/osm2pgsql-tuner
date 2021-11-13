a1bc393261ab679ff09ceafc89ab280b502331ab
Ryan Lambert <ryan@rustprooflabs.com>
2021-11-13 07:06:31 -0700

    Update tests for restructured code.

--- tests/osm2pgsql_test.py
+++ tests/osm2pgsql_test.py
@@ -1,6 +1,6 @@
 """ Unit tests to cover the pgconfig module."""
 import unittest
-from webapp import osm2pgsql
+from osm2pgsql_tuner import tuner
 
 
 SYSTEM_RAM_GB_MAIN = 64
@@ -17,77 +17,77 @@ class Osm2pgsqlTests(unittest.TestCase):
     ########################################
     # Class attribute types
     def test_osm2pgsql_recommendation_osm2pgsql_cache_max_type_float(self):
-        rec = osm2pgsql.recommendation(SYSTEM_RAM_GB_MAIN, OSM_PBF_GB_US)
+        rec = tuner.recommendation(SYSTEM_RAM_GB_MAIN, OSM_PBF_GB_US)
         result = rec.osm2pgsql_cache_max
         expected = float
         self.assertEqual(expected, type(result))
 
     def test_osm2pgsql_recommendation_osm2pgsql_noslim_cache_type_float(self):
-        rec = osm2pgsql.recommendation(SYSTEM_RAM_GB_MAIN, OSM_PBF_GB_US)
+        rec = tuner.recommendation(SYSTEM_RAM_GB_MAIN, OSM_PBF_GB_US)
         result = rec.osm2pgsql_noslim_cache
         expected = float
         self.assertEqual(expected, type(result))
 
     def test_osm2pgsql_recommendation_osm2pgsql_slim_cache_type_float(self):
-        rec = osm2pgsql.recommendation(SYSTEM_RAM_GB_MAIN, OSM_PBF_GB_US)
+        rec = tuner.recommendation(SYSTEM_RAM_GB_MAIN, OSM_PBF_GB_US)
         result = rec.osm2pgsql_slim_cache
         expected = float
         self.assertEqual(expected, type(result))
 
     def test_osm2pgsql_recommendation_osm2pgsql_run_in_ram_type_bool(self):
-        rec = osm2pgsql.recommendation(SYSTEM_RAM_GB_MAIN, OSM_PBF_GB_US)
+        rec = tuner.recommendation(SYSTEM_RAM_GB_MAIN, OSM_PBF_GB_US)
         result = rec.osm2pgsql_run_in_ram
         expected = bool
         self.assertEqual(expected, type(result))
 
     def test_osm2pgsql_recommendation_osm2pgsql_cache_mb_type_int(self):
-        rec = osm2pgsql.recommendation(SYSTEM_RAM_GB_MAIN, OSM_PBF_GB_US)
-        result = rec._get_cache_mb()
+        rec = tuner.recommendation(SYSTEM_RAM_GB_MAIN, OSM_PBF_GB_US)
+        result = rec.get_cache_mb()
         expected = int    
         self.assertEqual(expected, type(result))
 
 
 
     def test_osm2pgsql_recommendation_osm2pgsql_cache_max_value(self):
-        rec = osm2pgsql.recommendation(SYSTEM_RAM_GB_MAIN, OSM_PBF_GB_US)
+        rec = tuner.recommendation(SYSTEM_RAM_GB_MAIN, OSM_PBF_GB_US)
         result = rec.osm2pgsql_cache_max
         expected = 42.24
         self.assertEqual(expected, result)
 
     def test_osm2pgsql_recommendation_osm2pgsql_noslim_cache_value(self):
-        rec = osm2pgsql.recommendation(SYSTEM_RAM_GB_MAIN, OSM_PBF_GB_US)
+        rec = tuner.recommendation(SYSTEM_RAM_GB_MAIN, OSM_PBF_GB_US)
         result = rec.osm2pgsql_noslim_cache
         expected = 27.0
         self.assertEqual(expected, result)
 
     def test_osm2pgsql_recommendation_osm2pgsql_slim_cache_value(self):
-        rec = osm2pgsql.recommendation(SYSTEM_RAM_GB_MAIN, OSM_PBF_GB_US)
+        rec = tuner.recommendation(SYSTEM_RAM_GB_MAIN, OSM_PBF_GB_US)
         result = rec.osm2pgsql_slim_cache
         expected = 20.25
         self.assertEqual(expected, result)
 
     def test_osm2pgsql_recommendation_osm2pgsql_run_in_ram_value(self):
-        rec = osm2pgsql.recommendation(SYSTEM_RAM_GB_MAIN, OSM_PBF_GB_US)
+        rec = tuner.recommendation(SYSTEM_RAM_GB_MAIN, OSM_PBF_GB_US)
         result = rec.osm2pgsql_run_in_ram
         expected = True
         self.assertEqual(expected, result)
 
     def test_osm2pgsql_recommendation_osm2pgsql_cache_mb_value(self):
-        rec = osm2pgsql.recommendation(SYSTEM_RAM_GB_MAIN, OSM_PBF_GB_US)
-        result = rec._get_cache_mb()
+        rec = tuner.recommendation(SYSTEM_RAM_GB_MAIN, OSM_PBF_GB_US)
+        result = rec.get_cache_mb()
         expected = 20736
         self.assertEqual(expected, result)
 
 
     def test_osm2pgsql_recommendation_osm2pgsql_drop_value_false(self):
-        rec = osm2pgsql.recommendation(SYSTEM_RAM_GB_MAIN, OSM_PBF_GB_US)
+        rec = tuner.recommendation(SYSTEM_RAM_GB_MAIN, OSM_PBF_GB_US)
         result = rec.osm2pgsql_drop
         expected = False
         self.assertEqual(expected, result)
 
     def test_osm2pgsql_recommendation_osm2pgsql_drop_value_false_with_append(self):
         # NOTE: Setting system ram == PBF size to ensure slim is used
-        rec = osm2pgsql.recommendation(system_ram_gb=OSM_PBF_GB_US,
+        rec = tuner.recommendation(system_ram_gb=OSM_PBF_GB_US,
                                        osm_pbf_gb=OSM_PBF_GB_US,
                                        append=True)
         result = rec.osm2pgsql_drop
@@ -96,7 +96,7 @@ class Osm2pgsqlTests(unittest.TestCase):
 
     def test_osm2pgsql_recommendation_osm2pgsql_drop_value_true_without_append(self):
         # NOTE: Setting system ram == PBF size to ensure slim is used
-        rec = osm2pgsql.recommendation(system_ram_gb=OSM_PBF_GB_US,
+        rec = tuner.recommendation(system_ram_gb=OSM_PBF_GB_US,
                                        osm_pbf_gb=OSM_PBF_GB_US,
                                        append=False)
         result = rec.osm2pgsql_drop
@@ -104,14 +104,14 @@ class Osm2pgsqlTests(unittest.TestCase):
         self.assertEqual(expected, result)
 
     def test_osm2pgsql_recommendation_osm2pgsql_flat_nodes_value_false_with_plenty_of_ram(self):
-        rec = osm2pgsql.recommendation(system_ram_gb=SYSTEM_RAM_GB_MAIN,
+        rec = tuner.recommendation(system_ram_gb=SYSTEM_RAM_GB_MAIN,
                                        osm_pbf_gb=OSM_PBF_GB_US)
         result = rec.osm2pgsql_flat_nodes
         expected = False
         self.assertEqual(expected, result)
 
     def test_osm2pgsql_recommendation_osm2pgsql_flat_nodes_value_false_when_pbf_small(self):
-        rec = osm2pgsql.recommendation(system_ram_gb=SYSTEM_RAM_GB_SMALL,
+        rec = tuner.recommendation(system_ram_gb=SYSTEM_RAM_GB_SMALL,
                                        osm_pbf_gb=OSM_PBF_GB_CO)
         result = rec.osm2pgsql_flat_nodes
         expected = False
@@ -119,7 +119,7 @@ class Osm2pgsqlTests(unittest.TestCase):
 
 
     def test_osm2pgsql_recommendation_osm2pgsql_flat_nodes_value_true(self):
-        rec = osm2pgsql.recommendation(system_ram_gb=SYSTEM_RAM_GB_SMALL,
+        rec = tuner.recommendation(system_ram_gb=SYSTEM_RAM_GB_SMALL,
                                        osm_pbf_gb=OSM_PBF_GB_US)
         result = rec.osm2pgsql_flat_nodes
         expected = True
@@ -127,7 +127,7 @@ class Osm2pgsqlTests(unittest.TestCase):
 
 
     def test_osm2pgsql_recommendation_osm2pgsql_flat_nodes_value_true_no_ssd_when_pbf_gt30(self):
-        rec = osm2pgsql.recommendation(system_ram_gb=SYSTEM_RAM_GB_SMALL,
+        rec = tuner.recommendation(system_ram_gb=SYSTEM_RAM_GB_SMALL,
                                        osm_pbf_gb=OSM_PBF_GB_ALWAYS_FLAT_FILE,
                                        ssd=False)
         result = rec.osm2pgsql_flat_nodes
@@ -136,15 +136,15 @@ class Osm2pgsqlTests(unittest.TestCase):
 
 
     def test_osm2pgsql_recommendation_osm2pgsql_cache_mb_value_zero_with_flat_nodes(self):
-        rec = osm2pgsql.recommendation(SYSTEM_RAM_GB_SMALL, OSM_PBF_GB_US)
-        result = rec._get_cache_mb()
+        rec = tuner.recommendation(SYSTEM_RAM_GB_SMALL, OSM_PBF_GB_US)
+        result = rec.get_cache_mb()
         expected = 0
         self.assertEqual(expected, result)
 
 
     def test_osm2pgsql_recommendation_osm2pgsql_cache_mb_value_limited_ram_version(self):
-        rec = osm2pgsql.recommendation(SYSTEM_RAM_GB_SMALL, OSM_PBF_GB_USWEST)
-        result = rec._get_cache_mb()
+        rec = tuner.recommendation(SYSTEM_RAM_GB_SMALL, OSM_PBF_GB_USWEST)
+        result = rec.get_cache_mb()
         expected = 675
         self.assertEqual(expected, result)
 
