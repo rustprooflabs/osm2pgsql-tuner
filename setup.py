fe8b17bcc2d63e87af54810f64cde0cdd3950ee0
Ryan Lambert <ryan@rustprooflabs.com>
2021-12-03 15:44:11 -0700

    Bump version to next dev

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='osm2pgsql_tuner',
-      version='0.0.3',
+      version='0.0.4-dev',
       description='osm2pgsql tuner',
       url='https://github.com/rustprooflabs/osm2pgsql-tuner',
       author='RustProof Labs',
