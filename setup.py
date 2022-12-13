b24928515acb7cc522c8a34d1370be1ea76dba33
Ryan Lambert <ryan@rustprooflabs.com>
2022-12-12 20:12:40 -0700

    Bump verison for testing

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='osm2pgsql_tuner',
-      version='0.0.6-dev',
+      version='0.0.6-rc1',
       description='The osm2pgsql-tuner project recommends an osm2pgsql command based on available system resources and the size of the input PBF file.',
       url='https://github.com/rustprooflabs/osm2pgsql-tuner',
       author='RustProof Labs',
